47191065d30317588e0bac911ccdb666e9aefc9f
Lauren Kennedy <lauren.kennedy729@gmail.com>
2023-11-09 16:17:30 +1030

    added jobscript

---
+++ code/jobscript_sae_scores.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#SBATCH -p skylake
+#SBATCH --job-name=sae_scores
+#SBATCH --time=00:30:00
+#SBATCH --mail-user=lauren.a.kennedy@adelaide.edu.au
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mem-per-cpu=8000
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-100
+
+module use /apps/skl/modules/all/
+module load R/4.2.3-foss-2021b
+
+R CMD BATCH --no-save --no-restore score_model_sae.R reference_model_full_$SLURM_ARRAY_TASK_ID
